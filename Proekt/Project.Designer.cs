eb6f5860cd55b5d6e224b2b17cadeb5fc64a5e3e
Rustem <44618061+rustemium@users.noreply.github.com>
2019-05-16 18:17:32 +0300

    Add files via upload

---
+++ Proekt/Project.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Proekt
+{
+    partial class Project
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Project));
+            this.Сумма = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Сумма
+            // 
+            this.Сумма.AutoSize = true;
+            this.Сумма.Location = new System.Drawing.Point(747, 562);
+            this.Сумма.Name = "Сумма";
+            this.Сумма.Size = new System.Drawing.Size(0, 20);
+            this.Сумма.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(709, 562);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 20);
+            this.linkLabel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(250, 727);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add product";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(289, 655);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label1.MinimumSize = new System.Drawing.Size(300, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 60);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "What you need?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(422, 31);
+            this.label2.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label2.MinimumSize = new System.Drawing.Size(300, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 60);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "All product";
+            // 
+            // Project
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(688, 844);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Сумма);
+            this.MaximumSize = new System.Drawing.Size(710, 900);
+            this.MinimumSize = new System.Drawing.Size(710, 900);
+            this.Name = "Project";
+            this.Text = "Project";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Project_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.LinkLabel Сумма;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Proekt/Project.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Proekt
+{
+    public partial class Project : Form
+    {
+        private int i1 = 80;
+        private int i2 = 80;
+        public Project()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label lbl = new Label();
+            lbl.Location = new Point(52, i1); //расположение
+            lbl.Size = new Size(250, 30); // размер
+            lbl.Text = textBox1.Text; //текст
+            lbl.Font = new System.Drawing.Font("MV Boli", 13F);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            lbl.BackColor = Color.Transparent;
+            this.Controls.Add(lbl);
+            i1 += 30;
+            lbl.MouseDown += new MouseEventHandler(Lbl_MouseDown);
+        }
+
+        private void Lbl_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label clickedlabel = sender as Label;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                int x = clickedlabel.Location.X;
+                if (x == 52)
+                {
+                    clickedlabel.Location = new Point(320, i2);
+                    i1 -= 30;
+                    i2 += 30;
+                }
+                else
+                {
+                    clickedlabel.Location = new Point(52, i1);
+                    i1 += 30;
+                    i2 -= 30;
+                }
+
+            }
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                int x = clickedlabel.Location.X;
+                if (x == 52)
+                {
+                    this.Controls.Remove(clickedlabel);
+                    i1 -= 30;
+                }
+                else
+                {
+                    this.Controls.Remove(clickedlabel);
+                    i2 -= 30;
+                }
+            }
+        }
+
+        private void Lbl_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Project_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+        }
+
+    }
+}
